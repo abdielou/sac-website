dcb89571d99cf034f6fc794890102484b751f0a4
abdielou <abdiel.aviles@gmail.com>
2021-10-22 00:34:26 -0400

    Tailwind Nextjs Starter Blog

---
+++ data/headerNavLinks.js
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/about', title: 'Quiénes Somos' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Contáctenos' },
+  { href: '/projects', title: 'Condiciones del Tiempo' },
+  { href: '/projects', title: 'Membresía' },
+  { href: '/projects', title: 'Actividades' },
+]
+
+export default headerNavLinks
