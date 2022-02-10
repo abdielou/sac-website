3f979cd2c686e800dd639bda22decc99df40b8de
abdielou <abdiel.aviles@gmail.com>
2022-02-10 14:47:52 -0400

    [DRAFT] Membership and Events

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,9 @@ const headerNavLinks = [
   { href: '/about', title: 'Quiénes Somos' },
   { href: '/contact', title: 'Contáctenos' },
   { href: '/weather', title: 'Clima' },
+  { href: '/membership', title: 'Membresía' },
+  { href: '/events', title: 'Actividades' },
+  { href: '/blog', title: 'Blog' },
 ]
 
 export default headerNavLinks
---
+++ pages/events.js
@@ -0,0 +1,17 @@
+import { PageSEO } from '@/components/SEO'
+
+export default function Contact() {
+  return (
+    <>
+      <PageSEO title="Actividades" description="Noches de Observación Astronómica" />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Noches de Observación Astronómica
+          </h1>
+        </div>
+        <div>tbd</div>
+      </div>
+    </>
+  )
+}
