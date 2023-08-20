a20c9b8633bbddc1077eb81b8d9586ab3c4bd917
abdielou <abdiel.aviles@gmail.com>
2023-08-19 20:32:00 -0400

    Wait until mounted to render logo and title

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -7,9 +7,25 @@ import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 const LayoutWrapper = ({ children }) => {
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
+
   const { theme, resolvedTheme } = useTheme('dark')
+  const logoShort =
+    mounted &&
+    (theme === 'light' || resolvedTheme === 'light'
+      ? siteMetadata.siteLogoShortLight
+      : siteMetadata.siteLogoShortDark)
+  const logo =
+    mounted &&
+    (theme === 'light' || resolvedTheme === 'light'
+      ? siteMetadata.siteLogoLight
+      : siteMetadata.siteLogoDark)
+  const titleColor =
+    mounted && (theme === 'light' || resolvedTheme === 'light' ? 'text-sac-primary-blue' : '')
   return (
     <SectionContainer>
       <div className="flex flex-col justify-between h-screen">
@@ -18,42 +34,24 @@ const LayoutWrapper = ({ children }) => {
             <div className="flex items-center justify-between">
               <div>
                 <Link href="/" aria-label={siteMetadata.title}>
-                  <div className="flex items-center justify-between">
-                    <div className="mr-3 hidden sm:block">
-                      <Image
-                        src={
-                          theme === 'dark' || resolvedTheme === 'dark'
-                            ? siteMetadata.siteLogoShortDark
-                            : siteMetadata.siteLogoShortLight
+                  {mounted && (
+                    <div className="flex items-center justify-between">
+                      <div className="mr-3 hidden sm:block">
+                        <Image src={logoShort} alt="SAC Logo" width={200} height={47} />
+                      </div>
+                      <div className="mr-3 block sm:hidden">
+                        <Image src={logo} alt="SAC Logo" width={200} height={70} />
+                      </div>
+                      <div
+                        className={
+                          titleColor +
+                          ' hidden sm:block xl:pl-10 md:pl-4 h-10 xl:text-3xl lg:text-2xl md:text-2xl font-semibold italic tracking-tight xl:tracking-normal'
                         }
-                        alt="SAC Logo"
-                        width={200}
-                        height={47}
-                      />
+                      >
+                        {siteMetadata.headerTitle}
+                      </div>
                     </div>
-                    <div className="mr-3 block sm:hidden">
-                      <Image
-                        src={
-                          theme === 'dark' || resolvedTheme === 'dark'
-                            ? siteMetadata.siteLogoDark
-                            : siteMetadata.siteLogoLight
-                        }
-                        alt="SAC Logo"
-                        width={200}
-                        height={70}
-                      />
-                    </div>
-                    <div
-                      className={
-                        (theme === 'light' || resolvedTheme === 'light'
-                          ? 'text-sac-primary-blue'
-                          : '') +
-                        ' hidden sm:block xl:pl-10 md:pl-4 h-10 xl:text-3xl lg:text-2xl md:text-2xl font-semibold italic tracking-tight xl:tracking-normal'
-                      }
-                    >
-                      {siteMetadata.headerTitle}
-                    </div>
-                  </div>
+                  )}
                 </Link>
               </div>
               <ThemeSwitch />
