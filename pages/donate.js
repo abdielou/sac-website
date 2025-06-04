aa63022300e7abedcf22455513b40b129b383f83
Franz <franz.zbinden@upr.edu>
2025-06-04 18:03:52 -0400

    feature/71-switch-donation-buttons

--- pages/donate.js
+++ pages/donate.js
@@ -1,24 +1,8 @@
 import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
-import { useState, useEffect } from 'react'
 
 export default function Contact() {
-  const [isMobile, setIsMobile] = useState(false)
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const checkByAspectRatio = () => {
-        const aspectRatio = window.innerWidth / window.innerHeight
-        setIsMobile(aspectRatio < 0.8)
-      }
-
-      checkByAspectRatio()
-      window.addEventListener('resize', checkByAspectRatio)
-      return () => window.removeEventListener('resize', checkByAspectRatio)
-    }
-  }, [])
-
   return (
     <>
       <PageSEO
@@ -35,13 +19,14 @@ export default function Contact() {
           <h2 className="text-sm tracking-tight leading-5 text-gray-800 dark:text-gray-200 sm:text-lg sm:leading-6 md:text-xl md:leading-8">
             Sea parte del esfuerzo de la Sociedad de Astronomía del Caribe
           </h2>
-          <div className="pt-4 pb-4 prose dark:prose-dark max-w-none">
+          <div className="pt-4 pb-2 prose dark:prose-dark max-w-none">
             La Sociedad de Astronomía del Caribe es una organización sin fines de lucro 501(c)(3)
             que ofrece varias actividades educativas y de observación astronómica durante el año en
             diversas partes de la isla, libre de costo. Con tu donativo ayudas a la SAC a cumplir
             sus misión de difundir la ciencia de la astronomía en Puerto Rico, el Caribe y el mundo.
           </div>
-          <div className="visible md:invisible">
+          {/* ATH Movil */}
+          <div className="block md:hidden">
             <button
               className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue hover:bg-blue-700 dark:hover:bg-blue-500"
               onClick={() => window.open(siteMetadata.payments.payAthMovil, '_blank')}
@@ -49,20 +34,20 @@ export default function Contact() {
               Donativo con AthMovil
             </button>
           </div>
-          {!isMobile && (
-            <div>
-              <div className="pt-4 pb-4 prose dark:prose-dark max-w-none">
-                También puedes hacer tu donativo con ATH Movil desde tu celular.
-              </div>
-              <Image
-                src={siteMetadata.payments.payAthMovilQR}
-                alt="SAC ATH Movil"
-                width={400}
-                height={452}
-              />
+          {/* ATH Movil QR*/}
+          <div className="hidden md:block">
+            <div className="pt-2 pb-4 prose dark:prose-dark max-w-none">
+              También puedes hacer tu donativo con ATH Movil desde tu celular.
             </div>
-          )}
-          <div>
+            <Image
+              src={siteMetadata.payments.payAthMovilQR}
+              alt="SAC ATH Movil"
+              width={400}
+              height={452}
+            />
+          </div>
+          {/* PayPal button*/}
+          <div className="pt-2 pb-2 prose dark:prose-dark max-w-none">
             <button
               className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue hover:bg-blue-700 dark:hover:bg-blue-500"
               onClick={() => window.open(siteMetadata.payments.donatePaypal, '_blank')}
