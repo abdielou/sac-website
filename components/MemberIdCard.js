feaca0e15c1c802bab594f59482c4b2f68d2299c
Nader Suleiman <nader.suleiman@upr.edu>
2025-07-22 03:27:17 +0300

    [#81] Implemented website design

---
+++ components/MemberIdCard.js
@@ -0,0 +1,6 @@
+import { useEffect } from 'react'
+
+export default function MemberIdCard() {
+  useEffect(() => {}, [])
+  return null
+} 
--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,6 +4,7 @@ const headerNavLinks = [
   { href: '/weather', title: 'Clima' },
   { href: '/donate', title: 'Donaciones' },
   { href: '/membership', title: 'Membresía' },
+  { href: '/member-id-builder', title: 'Identificación' },
   { href: '/events', title: 'Actividades' },
   { href: '/blog', title: 'Blog' },
   { href: '/blog/telescopios', title: 'Guía de Telescopios' },
