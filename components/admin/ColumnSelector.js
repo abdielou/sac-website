110b9d7af001b6c9e3f2343a958898e3f4cf14be
abdielou <abdiel.aviles@gmail.com>
2026-02-19 15:43:15 -0400

    fix: suppress hydration warning for column count
    - Add suppressHydrationWarning to column count span
    - Fixes hydration mismatch when localStorage has different column preferences than server default

--- components/admin/ColumnSelector.js
+++ components/admin/ColumnSelector.js
@@ -91,7 +91,7 @@ export function ColumnSelector({ visibleColumnIds, onColumnToggle, onReset }) {
             d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2"
           />
         </svg>
-        <span>{visibleCount} columnas</span>
+        <span suppressHydrationWarning>{visibleCount} columnas</span>
       </button>
 
       {isOpen &&
